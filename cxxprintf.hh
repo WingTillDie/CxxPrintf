8e1feb5e7f8c2bea00382366f2ee09143444f236
WingTillDie <mychat.01a2bc3def@gmail.com>
2018-09-01 03:06:26 +0800

    Finished headerization, remove from std namespace, rename sprintf.cc to example.cc

---
+++ cxxprintf.hh
@@ -0,0 +1,20 @@
+#include <iostream>
+
+struct putf_r{
+	char *s;
+};
+
+putf_r putf(const char* fmt, ...){
+	va_list ap;
+	putf_r a;
+	va_start(ap, fmt);
+	vasprintf(&a.s, fmt, ap);
+	return a;
+}
+
+auto& operator<<(std::ostream& os, putf_r a){
+	std::cout<<a.s;
+	free(a.s);
+	return os;
+}
+
---
+++ example.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "cxxprintf.hh"
+
+int main(){
+	std::cout << putf("%3d", 23);
+}
