2673c20e9c0661dd31bb7e3922717f0bec561d58
WingTillDie <mychat.01a2bc3def@gmail.com>
2018-09-01 18:18:36 +0800

    Partial implemented coutf, gitignore .swp file

--- example.cc
+++ example.cc
@@ -1,7 +1,20 @@
 #include <iostream>
 #include "cxxprintf.hh"
 
+class ostreamf{
+private:
+	std::ostream *osp;
+public:
+	ostreamf(std::ostream &os_in){osp=&os_in;}
+	auto operator()(const char *fmt, ...){
+		*osp<<fmt;
+		return osp;
+	}
+};
+
 int main(){
 	std::cout << putf("%3d\n", 23) << putf("%a\n", 256.);
-	std::cerr << putf("%Le\n", LDBL_MAX) << putf("%p", NULL);
+	std::cerr << putf("%Le\n", DBL_MAX) << putf("%p", NULL);
+	ostreamf coutf(std::cout);//How to receive from cout
+	coutf("test");
 }
